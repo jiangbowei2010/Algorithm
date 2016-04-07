f788aaaf849b62e32b16f49507947dab642d78fd
jiangbowei2010 <luke.b.jiang@gmail.com>
2016-04-06 20:29:17 -0700

    add DFS of num of islands

--- src/Number_of_Islands.java
+++ src/Number_of_Islands.java
@@ -15,6 +15,10 @@
  */
 
 public class Number_of_Islands {
+	
+	int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+	
+	// Method 1: use Union Find
 	public int numIslandsUF(char[][] grid) {
 		if (grid == null || grid.length == 0 || grid[0].length == 0)
 			return 0;
@@ -55,13 +59,42 @@ public class Number_of_Islands {
 		}
 		return true;
 	}
+    
+	// Method 2: use DFS
+    public int numIslandsDFS(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    count++;
+                    dfs(grid, visited, i, j, m, n);
+                }
+            }
+        }
+        return count;
+    }
+    
+    private void dfs(char[][] grid, boolean[][] visited, int i, int j, int m, int n) {
+        visited[i][j] = true;
+        for (int[] dir : dirs) {
+            int x = i + dir[0], y = j + dir[1];
+            if (x < 0 || x >= m || y < 0 || y >= n) continue;
+            if (grid[x][y] != '1' || visited[x][y]) continue;
+            dfs(grid, visited, x, y, m, n);
+        }
+    }
 	
 	public static void main(String[] args) {
 		Number_of_Islands solution = new Number_of_Islands();
 		char[][] grid= {{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'0', '0', '1', '0', '0'}};
 		System.out.println(solution.numIslandsUF(grid)); //2
+		System.out.println(solution.numIslandsDFS(grid)); //2
 		grid = new char[][]{{}};
 		System.out.println(solution.numIslandsUF(grid)); //0
+		System.out.println(solution.numIslandsDFS(grid)); //0
 	}
 }
 
