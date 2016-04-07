240c9dff466f2c05431e9726d265a473d5be1f54
jiangbowei2010 <luke.b.jiang@gmail.com>
2016-04-06 21:47:17 -0700

    Graph Valid Tree

--- src/Graph_Valid_Tree.java
+++ src/Graph_Valid_Tree.java
@@ -42,6 +42,41 @@ public class Graph_Valid_Tree {
             else if (w != parent) hasCycle[0] = true;
         }
     }
+  
+    //Method 2: use Union Find
+    public boolean validTreeUF(int n, int[][] edges) {
+        if (n <= 0) return true;
+        int[] nums = new int[n];
+        int[] size = new int[n];
+        int count = n;
+        for (int i = 0; i < n; i++) nums[i] = i;
+        for (int[] edge : edges) {
+            int i = edge[0], j = edge[1];
+            while (nums[i] != i) i = nums[i];
+            while (nums[j] != j) j = nums[j];
+            if (i == j) return false;
+            count--;
+            if (size[i] > size[j]) {
+                nums[j] = i;
+                size[i] += size[j];
+            }
+            else {
+                nums[i] = j;
+                size[j] += size[i];
+            }
+        }
+        return count == 1;
+    }
+    
+    public static void main(String[] args) {
+    	Graph_Valid_Tree solution = new Graph_Valid_Tree();
+    	int[][] edges = {{0, 1}, {0, 2}, {0, 3}, {1, 4}};
+    	System.out.println(solution.validTreeDFS(5, edges)); //true
+    	System.out.println(solution.validTreeUF(5, edges)); //true
+    	edges = new int[][]{{0, 1}, {1, 2}, {2, 3}, {1, 3}, {1, 4}};
+    	System.out.println(solution.validTreeDFS(5, edges)); //false
+    	System.out.println(solution.validTreeUF(5, edges)); //false
+    }
 }
 
 
